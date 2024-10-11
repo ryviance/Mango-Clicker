abcdc1de7a45b8f91385af33746cf12a4132c7a1
ryviance <eion.ling0@gmail.com>
2024-10-10 20:06:49 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,10 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// Create a button element with a mango emoji
+const button = document.createElement("button");
+button.innerHTML = "Start Game 🥭"; 
+button.style.marginTop = "20px"; 
+
+app.append(button);
