6bb3e8584679b7369193ce1d7b822fca3e80423f
ryviance <eion.ling0@gmail.com>
2024-10-10 20:16:46 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,29 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Eion's amazing game!!!";
+const gameName = "Mango farm";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-// Create a button element with a mango emoji
+// Counter
+
+let counter: number = 0;
+
+const counterDiv = document.createElement("div");
+counterDiv.innerHTML = `${counter} mangoes`;
+counterDiv.style.marginTop = "20px";
+app.append(counterDiv);
+
+// Mango Emoji
 const button = document.createElement("button");
 button.innerHTML = "Start Game 🥭"; 
 button.style.marginTop = "20px"; 
-
 app.append(button);
+
+button.addEventListener("click", ()=> {
+    counter++;
+    counterDiv.innerHTML = `${counter} mangoes`; 
+})
